2a0013100dc5c710828fc5af2da078b7f31d6f0d
Mihail <m.a.stepanov@outlook.com>
2024-07-14 16:42:50 +0300

    Initial commit

---
+++ BackendAspire/frontend/vite.config.ts
@@ -0,0 +1,13 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port:  parseInt(process.env.PORT ?? "5173"),
+  }
+})
